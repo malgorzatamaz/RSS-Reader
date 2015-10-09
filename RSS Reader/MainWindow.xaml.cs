fefee40d82ec7772e6c7f796c1ee14345f1e1c25
malgorzatamaz <malgorzatamaz@gmail.com>
2015-10-09 18:27:03 +0200

    Combobox działa, da się zmienić kanały :) :) :D :P :D

--- RSS Reader/MainWindow.xaml.cs
+++ RSS Reader/MainWindow.xaml.cs
@@ -1,9 +1,12 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
 using System.IO;
 using System.Linq;
 using System.Net;
 using System.Web;
 using System.Windows;
+using System.Windows.Data;
 using HtmlAgilityPack;
 using MahApps.Metro.Controls;
 using RSS_Reader.Models;
@@ -16,17 +19,24 @@ namespace RSS_Reader
     public partial class MainWindow : MetroWindow
     {
         public List<Category> Categories { get; set; }
-        public string CategoryPath { get; set; }
+        public event PropertyChangedEventHandler PropertyChanged;
 
+        private string _categoryPath;
+        public string CategoryPath
+        {
+            get { return _categoryPath; }
+            set
+            {
+                _categoryPath = value;
+                OnPropertyChanged("CategoryPath");
+            }
+        }
+        
         public MainWindow()
         {
-            Categories = new List<Category>();
-            Categories.Add(new Category { Name = "dadsd", Url = "www.sssa.pl" });
-            Categories.Add(new Category { Name = "ada", Url = "www.dads.pl" });
-            
             InitializeComponent();
-            CbxCanals.DataContext = Categories;
-
+            Categories = GetCategories();
+            CbxCategory.DataContext = Categories;
         }
 
         private void Button_Click(object sender, RoutedEventArgs e)
@@ -39,22 +49,51 @@ namespace RSS_Reader
             HttpUtility.HtmlDecode(entry.Description, myWriter);
             string decode = myWriter.ToString(); // potrzebny nam tekst mamy w <p>...</p>         
         }
-        private void GetCategories()
+        private List<Category> GetCategories()
         {
+            List<Category> list = new List<Category>();
+            var listTitles = new List<string>();
+            var listUrls = new List<string>();
+
             WebClient webClient = new WebClient();
             string page = webClient.DownloadString("http://rss.interia.pl/");
-            HtmlAgilityPack.HtmlDocument doc = new HtmlAgilityPack.HtmlDocument();
+            HtmlAgilityPack.HtmlDocument doc = new HtmlDocument();
             doc.LoadHtml(page);
-            
-            foreach (HtmlNode table in doc.DocumentNode.SelectNodes("//table"))
-            {   
-                foreach (HtmlNode row in table.SelectNodes("tr"))
-                {
-                    Categories.Add( new Category{ Name = row.SelectNodes("td")[0].ToString(), Url = row.SelectNodes("td")[1].ToString()});
-                }
+
+            var titles = from node in doc.DocumentNode.SelectNodes("//td[@class = 'tdPolecane01']")
+                                select node.InnerText.ToString();
+
+            listTitles = titles.AsQueryable().ToList();
+
+            var urls = from node in doc.DocumentNode.SelectNodes("//td[@class = 'tdPolecane02']")
+                       select node.InnerText.ToString();
+
+            listUrls = urls.AsQueryable().ToList();
+
+            for (int i = 0; i < listTitles.Count(); i++)
+            {
+                list.Add(new Category { Name = listTitles[i], Url = listUrls[i] });
             }
+            return list;
         }
 
+        protected void OnPropertyChanged(PropertyChangedEventArgs e)
+        {
+            PropertyChangedEventHandler handler = PropertyChanged;
+            if (handler != null)
+                handler(this, e);
+        }
+
+        protected void OnPropertyChanged(string propertyName)
+        {
+            OnPropertyChanged(new PropertyChangedEventArgs(propertyName));
+        }
+
+        private void CbxCategory_SelectionChanged(object sender, System.Windows.Controls.SelectionChangedEventArgs e)
+        {
+            var provider = (XmlDataProvider)this.Resources["rssData"];
+            provider.Source = new Uri(Categories[CbxCategory.SelectedIndex].Url); 
+        }
     }
 
    
