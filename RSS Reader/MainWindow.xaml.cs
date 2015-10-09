26664f5b006989d5d0ac78e8627f9ff716133497
malgorzatamaz <malgorzatamaz@gmail.com>
2015-10-09 14:01:48 +0200

    ....

--- RSS Reader/MainWindow.xaml.cs
+++ RSS Reader/MainWindow.xaml.cs
@@ -1,7 +1,10 @@
 ﻿using System.Collections.Generic;
 using System.IO;
+using System.Linq;
+using System.Net;
 using System.Web;
 using System.Windows;
+using HtmlAgilityPack;
 using MahApps.Metro.Controls;
 using RSS_Reader.Models;
 
@@ -10,11 +13,19 @@ namespace RSS_Reader
     /// <summary>
     /// Interaction logic for MainWindow.xaml
     /// </summary>
-    public partial class MainWindow :MetroWindow
+    public partial class MainWindow : MetroWindow
     {
+        public List<Category> Categories { get; set; }
+        public string CategoryPath { get; set; }
+
         public MainWindow()
         {
+            Categories = new List<Category>();
+            Categories.Add(new Category { Name = "dadsd", Url = "www.sssa.pl" });
+            Categories.Add(new Category { Name = "ada", Url = "www.dads.pl" });
+            
             InitializeComponent();
+            this.DataContext = Categories;
         }
 
         private void Button_Click(object sender, RoutedEventArgs e)
@@ -27,9 +38,22 @@ namespace RSS_Reader
             HttpUtility.HtmlDecode(entry.Description, myWriter);
             string decode = myWriter.ToString(); // potrzebny nam tekst mamy w <p>...</p>         
         }
+        private void GetCategories()
+        {
+            WebClient webClient = new WebClient();
+            string page = webClient.DownloadString("http://rss.interia.pl/");
+            HtmlAgilityPack.HtmlDocument doc = new HtmlAgilityPack.HtmlDocument();
+            doc.LoadHtml(page);
+            
+            foreach (HtmlNode table in doc.DocumentNode.SelectNodes("//table"))
+            {   
+                foreach (HtmlNode row in table.SelectNodes("tr"))
+                {
+                    Categories.Add( new Category{ Name = row.SelectNodes("td")[0].ToString(), Url = row.SelectNodes("td")[1].ToString()});
+                }
+            }
+        }
 
-
-        public List<Category> Categories { get; set; }
     }
 
    
--- RSS Reader/Models/Category.cs
+++ RSS Reader/Models/Category.cs
@@ -8,7 +8,6 @@ namespace RSS_Reader.Models
 {
     public class Category
     {
-        public int Id { get; set; }
         public string Name { get; set; }
         public string Url { get; set; }
     }
